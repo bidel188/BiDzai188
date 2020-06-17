a3809441360781c4d0e0bc966e672c56d43170a5
bidel188 <66129030+bidel188@users.noreply.github.com>
2020-06-17 21:32:29 +0700

    Expense 2

---
+++ src/JavaCore/Expense.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Expense {
+    static int lastId=0;
+    private int ID; 
+    private String date, content;
+    private double amount;
+
+    public Expense() {
+    }
+
+    public Expense( String date, double amount, String content) {
+        this.ID = ++lastId;
+        this.date = date;
+        this.amount = amount;
+        this.content = content;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+    @Override
+    public String toString() {
+        String out = String.format("%-4d%-15s%-15.2f%-20s", ID, date, amount, content);
+        return out;
+    }
+
+
+}
